a251c170f7b51a64bc2465a41bcd5254c31258e0
nico0715 <147874351+nico0715@users.noreply.github.com>
2023-12-01 23:38:36 -0700

    added java

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// JavaScript Documentconst myHeading = document.querySelector("h1");
+console.log("hello World");
+console.log(new Date().getFullYear());
+document.getElementById('footer').innerHTML=("Solar psychic services©")+(new Date().getFullYear())
+function myFunction() {
+  alert("sorry, unable to connect with the dead");}
