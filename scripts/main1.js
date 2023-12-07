2a2dcec09bd5e522dba5ac4454f08007eb01b3c6
nico0715 <147874351+nico0715@users.noreply.github.com>
2023-12-06 21:26:15 -0700

    counter btnn

--- scripts/main1.js
+++ scripts/main1.js
@@ -4,9 +4,3 @@ console.log(new Date().getFullYear());
 document.getElementById('footer').innerHTML=("Solar psychic services©")+(new Date().getFullYear())
 function myFunction() {
   alert("sorry, unable to connect with the dead");}
-var clicks = 0;
-
-function onClick() {
-  clicks += 1;
-  document.getElementById("clicks").innerHTML = clicks;
-};
